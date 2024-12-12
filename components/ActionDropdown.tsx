93fa15efe3c1348896ff1505ab31558a8e20835d
massi <massi@MacBook-Air-de-massi.local>
2024-12-12 20:49:53 +0100

    implement global search and filter

--- components/ActionDropdown.tsx
+++ components/ActionDropdown.tsx
@@ -25,7 +25,7 @@ import Link from "next/link"
 import { constructDownloadUrl } from "@/lib/utils"
 import { Input } from "./ui/input"
 import { Button } from "./ui/button"
-import { renameFile, updateFileUsers } from "@/lib/actions/file.actions"
+import { deleteFile, renameFile, updateFileUsers } from "@/lib/actions/file.actions"
 import { usePathname } from "next/navigation"
 import { FileDetails, ShareInput } from "./ActionsModalContent"
 
@@ -61,7 +61,11 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
         path,
         emails
       }),
-      delete: () => { }
+      delete: () => deleteFile({
+        fileId: file.$id,
+        path,
+        bucketFileId: file.bucketFileId
+      })
     }
 
     success = await actions[action.value as keyof typeof actions]();
@@ -100,8 +104,20 @@ const ActionDropdown = ({ file }: { file: Models.Document }) => {
             />
           )}
           {value === "details" && <FileDetails file={file} />}
-          {value === "share" && <ShareInput file={file} onInputChange={setEmails}
-            onRemove={handleRemoveUser} />}
+          {value === "share" && (
+            <ShareInput
+              file={file}
+              onInputChange={setEmails}
+              onRemove={handleRemoveUser}
+            />
+          )
+          }
+          {value === "delete" && (
+            <p className="delete-confirmation">
+              Are you sure you want to delete
+              <span className="delete-file-name"> {file.name}</span> ?
+            </p>
+          )}
         </DialogHeader>
         {["rename", "delete", "share"].includes(value) && (
           <DialogFooter className="flex flex-col gap-3 md:flex-row">
--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -58,18 +58,40 @@ export const uploadFile = async ({
   }
 };
 
-const createQueries = (currentUser: Models.Document) => {
+const createQueries = (
+  currentUser: Models.Document,
+  types: FileType[],
+  searchText: string,
+  sort: string,
+  limit?: number
+) => {
   const queries = [
     Query.or([
       Query.equal("owner", [currentUser.$id]),
       Query.contains("users", [currentUser.email]),
     ]),
   ];
+  if (types.length > 0) queries.push(Query.equal("type", types));
+  if (searchText) queries.push(Query.contains("name", searchText));
+  if (limit) queries.push(Query.limit(limit));
+
+  if (sort) {
+    const [sortBy, orderBy] = sort.split("-");
+    queries.push(
+      orderBy === "asc" ? Query.orderAsc(sortBy) : Query.orderDesc(sortBy)
+    );
+  }
+
   //TODO sort, limit, search ...
   return queries;
 };
 
-export const getFiles = async () => {
+export const getFiles = async ({
+  types = [],
+  searchText = "",
+  sort = "$createdAt-desc",
+  limit,
+}: GetFilesProps) => {
   const { databases } = await createAdminClient();
   try {
     const currentUser = await getCurrentUser();
@@ -77,7 +99,7 @@ export const getFiles = async () => {
       throw new Error("User not found.");
     }
 
-    const queries = createQueries(currentUser);
+    const queries = createQueries(currentUser, types, searchText, sort, limit);
     const files = await databases.listDocuments(
       appwriteConfig.databaseId,
       appwriteConfig.filesCollectionId,
@@ -116,7 +138,7 @@ export const renameFile = async ({
 export const updateFileUsers = async ({
   fileId,
   path,
-  emails
+  emails,
 }: UpdateFileUsersProps) => {
   const { databases } = await createAdminClient();
   try {
@@ -131,5 +153,29 @@ export const updateFileUsers = async ({
     return parseStringify(updatedFile);
   } catch (err) {
     handleError(err, "Failed to share the file.");
+  }
+};
+
+export const deleteFile = async ({
+  fileId,
+  path,
+  bucketFileId,
+}: DeleteFileProps) => {
+  const { databases, storage } = await createAdminClient();
+  try {
+    const deletedFile = await databases.deleteDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.filesCollectionId,
+      fileId
+    );
+
+    if (deletedFile) {
+      await storage.deleteFile(appwriteConfig.bucketId, bucketFileId);
+    }
+
+    revalidatePath(path);
+    return parseStringify({ status: "success" });
+  } catch (err) {
+    handleError(err, "Failed to delete the file.");
   }
 };
